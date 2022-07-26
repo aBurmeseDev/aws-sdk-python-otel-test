b5f4af6f1184b4d73cdca7a38ec06da2da5eb46a
John Lwin <johnlwin@amazon.com>
2022-07-26 13:45:48 -0700

    add tracing

--- src/tracing.py
+++ src/tracing.py
@@ -1,13 +1,29 @@
-# Basic packages for your application
 import boto3
-from flask import Flask
-import json
-
-# Add imports for OTel components into the application
 from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
-
-# Import the AWS X-Ray for OTel Python IDs Generator into the application.
-from opentelemetry.sdk.extension.aws.trace import AwsXRayIdsGenerator
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
+from opentelemetry.sdk.resources import get_aggregated_resources
+from opentelemetry.sdk.extension.aws.resource.ec2 import (
+    AwsEc2ResourceDetector,
+)
+trace.set_tracer_provider(
+    TracerProvider(
+        resource=get_aggregated_resources(
+            [
+                AwsEc2ResourceDetector(),
+            ]
+        ),
+    )
+)
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(ConsoleSpanExporter())
+)
+tracer = trace.get_tracer(__name__)
+with tracer.start_as_current_span("foo"):
+    ec2 = boto3.client('ec2')
+    ec2.region = 'us-east-2'
+    response = ec2.describe_instances()
+    print('Response: ', response)
