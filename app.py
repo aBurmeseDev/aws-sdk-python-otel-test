3afbca1027323c867356aa2da5305a3ede292a61
John Lwin <johnlwin@amazon.com>
2022-07-27 15:43:18 -0700

    add JaegarExporter and ListTables on DynamoDB client

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import boto3
 import instrumentation
+from time import sleep
 # from pprint import pprint
 
-## boto3 list buckets client method
+# boto3 listBuckets client method
 if __name__ == "__main__":
     s3 = boto3.client("s3")
     s3.region = "us-east-2"
     response = s3.list_buckets()
 
     print("Response: ", response)
+
+# # DynamoDB listTables client method
+# if __name__ == "__main__":
+#     client = boto3.client("dynamodb")
+#     client.region = "us-east-2"
+
+#     for i in range(20):
+#         response = client.list_tables()
+#         sleep(1)
+
+#         print("Response: ", response)
--- instrumentation/__init__.py
+++ instrumentation/__init__.py
@@ -2,9 +2,24 @@ from opentelemetry import propagate, trace
 from opentelemetry.instrumentation.botocore import BotocoreInstrumentor
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import Resource
 import boto3
 
-span_processor = BatchSpanProcessor(ConsoleSpanExporter())
+# create a JaegerExporter
+jaeger_exporter = JaegerExporter(
+    # configure agent
+    agent_host_name='localhost',
+    agent_port=6831,
+    # optional: configure also collector
+    # collector_endpoint='http://localhost:14268/api/traces?format=jaeger.thrift',
+    # username=xxxx, # optional
+    # password=xxxx, # optional
+    # max_tag_value_length=None # optional
+)
+
+
+span_processor = BatchSpanProcessor(jaeger_exporter)
 trace.set_tracer_provider(
     TracerProvider(
         active_span_processor=span_processor,
@@ -13,6 +28,10 @@ trace.set_tracer_provider(
         #         AwsEc2ResourceDetector(),
         #     ]
         # ),
+         resource=Resource.create({
+            "service.name": "pythonOtelSDK",
+            "service.instance.id": "instance-12",
+        }),
     )
 )
 
